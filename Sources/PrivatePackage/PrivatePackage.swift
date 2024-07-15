b02e5924448b5cc2d3d4a24aa480db0c3b5d34d3
Uto <daffamadeo@gmail.com>
2024-07-15 20:29:09 +0700

    Update PrivatePackage.swift

--- Sources/PrivatePackage/PrivatePackage.swift
+++ Sources/PrivatePackage/PrivatePackage.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public class PrivateMan {
+open class PrivateMan {
     public func sayHey() {
         print("hey")
     }

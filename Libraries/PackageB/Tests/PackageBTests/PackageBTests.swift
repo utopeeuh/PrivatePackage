27ba6510b46c02cad732786a5489914223481b12
Uto <daffamadeo@gmail.com>
2024-07-22 14:01:34 +0700

    jjj

---
+++ Libraries/PackageB/Tests/PackageBTests/PackageBTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import PackageB
+
+final class PackageBTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
